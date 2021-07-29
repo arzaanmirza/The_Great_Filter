a7b4db25e7e4dd0209816cb030207142c34e62c2
Arzaan Mirza <arzaanmirza@Arzaans-Air.modem>
2021-07-29 18:32:35 +1000

    updated flask files, main website is set up. Just need to return the results from the script files

--- flask/views.py
+++ flask/views.py
@@ -1,6 +1,25 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/', methods=["POST","GET"])
+def search():
+    if request.method == "POST":
+        country = request.form["query"]
+        return redirect(url_for("country_page", name_of_country=country))
+    else:
+        return render_template("home.html")
+
+
+@app.route("/<name_of_country>", methods=["POST","GET"])
+def country_page(name_of_country):
+    print(name_of_country)
+    if request.method == "POST":
+        country = request.form["query"]
+        return redirect(url_for("country_page", name_of_country=country))
+    else:
+        return render_template("index.html")
+
+# You are able to take the function call of the country name. Now you have to take the country
+# and put it through the functions that you have created to return results. Once you have returned the results
+# you have to find a way to display them on a html file. 
