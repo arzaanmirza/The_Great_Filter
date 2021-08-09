81da11821d9ab16bb1bed56b04b963071d47bbdc
Arzaan Mirza <arzaanmirza@Arzaans-Air.modem>
2021-08-09 18:24:33 +1000

    updates of news filter code

--- get_climatechange_news.py
+++ get_climatechange_news.py
@@ -4,44 +4,62 @@
 #import requests
 import json
 from newsapi import NewsApiClient
+from image_capture_database import image_capture_database
 
-api = NewsApiClient(api_key='cc17316ba8b84f2eb4aa4d76a6b7d1a5')
+def get_climatechange_news(country_inputted):
 
-country_inputted = 'Mexico'    # Country chosen
-#disaster_words = '(kills OR deaths OR destruction)'#disaster words
+    api = NewsApiClient(api_key='cc17316ba8b84f2eb4aa4d76a6b7d1a5')
+    #country_inputted = 'Mexico'    # Country chosen
+    #disaster_words = '(kills OR deaths OR destruction)'#disaster words
 
-query_string = "("+country_inputted + ' AND ' + "climate change"+")" #+ ' OR ' + disaster_words # Query statement
-#news_sources='bbc-news,reuters,abc-news,cnn,foxnews,times-of-india,the-guardian' #All the news sources where you want to search from
+    query_string = "("+country_inputted + ' AND ' + "climate change"+")" #+ ' OR ' + disaster_words # Query statement
+    #news_sources='bbc-news,reuters,abc-news,cnn,foxnews,times-of-india,the-guardian' #All the news sources where you want to search from
 
-data = api.get_everything(q=query_string)
+    data = api.get_everything(q=query_string)
 
-list_of_articles = data["articles"]
+    list_of_articles = data["articles"]
+    list_of_related_articles = []
+    count = 0
+    for article in list_of_articles:
 
-count = 0
-for article in list_of_articles:
-
-    title = article["title"]         # Finds the name of the article title
-    media_source = article["source"]["name"] # Finds the name of the media source
-    article_description = article["description"] #Finds the description of the article
-    article_url = article["url"]          #Finds the url of the article
+        title = article["title"]         # Finds the name of the article title
+        media_source = article["source"]["name"] # Finds the name of the media source
+        article_description = article["description"] #Finds the description of the article
+        article_url = article["url"]          #Finds the url of the article
+        image_inside_article_url = image_capture_database(article["title"])
 
 
-#This if-statement ensures that the country is in the description
-# of the article, this ensures that the news is from the country inputted.
-    if country_inputted not in article_description:
+    #This if-statement ensures that the country is in the description
+    # of the article, this ensures that the news is from the country inputted.
+        if country_inputted not in article_description:
+                continue
+
+        if "climate change" not in article_description:
             continue
 
-    if "climate change" not in article_description:
-        continue
+    # Printing commands:
+        # print(media_source) # Finds the name of the media source
+        # print(f"Article Title: {title} ")
+        # print(f"Description: {article_description} ")
+        # print(f"Article URL: {article_url} ") #Prints the url of the article
 
-# Printing commands:
-    print(media_source) # Finds the name of the media source
-    print(f"Article Title: {title} ")
-    print(f"Description: {article_description} ")
-    print(f"Article URL: {article_url} ") #Prints the url of the article
+            data = {
+
+                "Media": media_source,
+                "Article Title": title,
+                "Description": article_description,
+                "Article URL": article_url,
+                "Image URL": image_inside_article_url,
+
+            }
+            print(data["Image URL"])
+            # print(data)
+            list_of_related_articles.append(data)
 
 
-    count = count + 1
-    print("\n")
+        count = count + 1
+        print("\n")
 
-print(count)
+    return list_of_related_articles
+
+    #print(count)
