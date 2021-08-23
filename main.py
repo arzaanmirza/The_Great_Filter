cd9b87df7d2ff49a106531cd14dd0646b4896966
Arzaan Mirza <78077697+arzaanmirza@users.noreply.github.com>
2021-08-23 23:31:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from app import app
 import views
 
 if __name__ == '__main__':
-    #app.run(port=8965)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=8965)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
