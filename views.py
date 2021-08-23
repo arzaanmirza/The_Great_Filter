956abf567ba6a1d06c6700950cd2f1b59161f9ab
Arzaan Mirza <78077697+arzaanmirza@users.noreply.github.com>
2021-08-23 23:42:36 +1000

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,3 @@
-
 from flask import render_template, request, redirect, url_for
 from get_climatechange_news import get_climatechange_news
 from get_naturaldisaster_news import get_naturaldisaster_news
