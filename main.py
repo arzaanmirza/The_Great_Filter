953a6a72c4e1b8f69bcc81c72e97a51c34b6f5ef
Arzaan Mirza <arzaanmirza@Arzaans-Air.modem>
2021-08-23 23:15:55 +1000

    updates

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from app import app
 import views
 
 if __name__ == '__main__':
-    app.run(port=8965)
+    #app.run(port=8965)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
