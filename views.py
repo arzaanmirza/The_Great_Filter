a755fd4617bdfd44f029745159e34091c17a19d1
Arzaan Mirza <78077697+arzaanmirza@users.noreply.github.com>
2021-08-23 23:37:58 +1000

    Update views.py

--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from get_climatechange_news import get_climatechange_news
 from get_naturaldisaster_news import get_naturaldisaster_news
 import json
 
+app = Flask(__name__)
+
 @app.route('/', methods=["POST","GET"])
 def search():
     if request.method == "POST":
@@ -34,3 +36,9 @@ def country_page(name_of_country):
         return redirect(url_for("country_page", name_of_country=country))
     else:
         return render_template("index.html",country=name_of_country,data=data_to_send)
+    
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
